b620ac05deb7c4883424049c6bcfd45eacc79d96
FireballGIT <gafutb@outlook.com>
2025-12-27 14:26:07 -0600

    Extension template for CalcuFunction

---
+++ bmods/tests and templates/CalcuFunctionExt.py
@@ -0,0 +1,14 @@
+# CalcuFunction extension template
+
+pi = 3.14
+class CalcuFunction:
+  class YourModule:
+    @staticmethod
+    def circleArea(radius):
+      """Calculate circle area"""
+      r2 = radius ** 2
+      return pi * r2
+    @staticmethod
+    """Calculate rectangular prism area"""
+    def rectangularPrismArea(h, w, l):
+      return l * w * h
